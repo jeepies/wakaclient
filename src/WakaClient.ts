0caf8b7095446946fd95dfd0d718cac8738c6f00
jay <davisjayalan@gmail.com>
2024-11-12 02:37:50 +0000

    merged all files into a singular file

--- src/WakaClient.ts
+++ src/WakaClient.ts
@@ -1,6 +1,13 @@
 import axios, { AxiosInstance } from "axios";
-import Range from "./Range";
 import { AxiosResponse } from "../node_modules/axios/index";
+
+enum Range {
+  LAST_7_DAYS = "last_7_days",
+  LAST_30_DAYS = "last_30_days",
+  LAST_6_MONTHS = "last_6_months",
+  LAST_YEAR = "last_year",
+  ALL_TIME = "all_time",
+}
 
 class WakaClient {
   private client: AxiosInstance;
